508b3ad0b6a7c6bbc2fceb5065c476769719096f
AbuBokorprog <abubokor1066@gmail.com>
2023-10-23 22:55:56 +0600

    Best Blogs

--- src/pages/home/best-blogs.tsx
+++ src/pages/home/best-blogs.tsx
@@ -1,11 +1,42 @@
-// import React from 'react';
-
+import axios from "axios";
 import Container from "../../components/ui/container";
+import { useEffect, useState } from "react";
+import Blogs from "./blogs";
+interface BestBlogs {
+  thumbnail: string;
+  title: string;
+  description: string;
+  authorName: string;
+  authorProfile: string;
+  publishDate: string;
+}
 
-const BestBlogs = () => {
+const BestBlogs: React.FC = () => {
+  const [bestBlogs, setBestBloga] = useState<BestBlogs[]>([]);
+  useEffect(() => {
+    axios
+      .get("/db/best-blogs.json")
+      .then(({ data }) => {
+        setBestBloga(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  // console.log();
   return (
-    <Container>
-      <div></div>
+    <Container className="lg:py-20">
+      <h2 className="mx-auto text-center">Discover Our Latest Blog Entries</h2>
+      <p className="px-4 lg:px-16 text-center py-2 font-normal">
+        Explore our blog section for captivating articles on travel, culture,
+        and adventure. Get inspired and stay informed with our diverse
+        collection of stories.
+      </p>
+      <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3 mx-auto">
+        {bestBlogs.map((b, index) => (
+          <Blogs key={index} {...b} />
+        ))}
+      </div>
     </Container>
   );
 };
