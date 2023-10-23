b39d9309d717da2dda0a8d5972a74fde3c2ccf74
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-10-23 17:04:21 +0600

    replace banner.css to tailwindcss

--- src/pages/home/banner.tsx
+++ src/pages/home/banner.tsx
@@ -1,14 +1,12 @@
 // import Swiper styles
 
 import Container from "../../components/ui/container";
-import "./banner.css";
 
 // import Button from "../../components/ui/button";
 const Banner = () => {
   return (
     <Container>
-      {/*  */}
-      <div className="img md:py-32 lg:py-56">
+      <div className="bg-[url('https://cdn.pixabay.com/photo/2019/12/26/03/51/hotel-4719609_1280.jpg')] bg-cover bg-no-repeat bg-center md:py-32 lg:py-56">
         <div className="lg:pl-6 py-20 lg:py-0">
           <h1 className="text-white">
             Discover <br /> Tranquility at
