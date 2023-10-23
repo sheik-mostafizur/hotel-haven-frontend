d8789d35e39b4f36e16c75d55aacf642e633177d
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-10-23 13:00:03 +0600

    add some data in public files

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -10,13 +10,18 @@ type ButtonType = {
 const Button = (props: ButtonType) => {
   const {children, size = "default", className, type} = props;
 
+  const mainColor = "bg-primary-500";
+  const hoverColor = "hover:bg-primary-600";
+  const focusColor = "focus:ring-4 focus:outline-none focus:ring-primary-300";
+  const darkColor =
+    "dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800";
+
   const buttonStyle = {
-    xs: "px-3 py-2 text-xs font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800",
-    sm: "px-3 py-2 text-sm font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800",
-    default:
-      "px-5 py-2.5 text-sm font-medium text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800",
-    lg: "px-5 py-3 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800",
-    xl: "px-6 py-3.5 text-base font-medium text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800",
+    xs: `px-3 py-2 text-xs font-medium text-center text-white ${mainColor} rounded-lg ${hoverColor} ${focusColor} ${darkColor}`,
+    sm: `px-3 py-2 text-sm font-medium text-center text-white ${mainColor} rounded-lg ${hoverColor} ${focusColor} ${darkColor}`,
+    default: `px-5 py-2.5 text-sm font-medium text-white ${mainColor} ${hoverColor} ${focusColor} rounded-lg text-center ${darkColor}`,
+    lg: `px-5 py-3 text-base font-medium text-center text-white ${mainColor} rounded-lg ${hoverColor} ${focusColor} ${darkColor}`,
+    xl: `px-6 py-3.5 text-base font-medium text-white ${mainColor} ${hoverColor} ${focusColor} rounded-lg text-center ${darkColor}`,
   };
 
   return (
