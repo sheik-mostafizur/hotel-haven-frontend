1331e4dc0247e9f13bde0f4fa7acabd65486108d
AbuBokorprog <abubokor1066@gmail.com>
2023-10-23 13:00:02 +0600

    Banner Half done

--- src/pages/home/banner.tsx
+++ src/pages/home/banner.tsx
@@ -3,7 +3,13 @@
 const Banner = () => {
   return (
     <>
-      <div></div>
+      <div>
+        <img
+          className="w-screen max-h-screen"
+          src="https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_1280.jpg"
+          alt=""
+        />
+      </div>
     </>
   );
 };
