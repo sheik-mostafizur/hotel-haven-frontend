6832ee19135dc0e5c42e4811a4c36b5dba75ba4d
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-10-26 13:21:59 +0600

    admin see user data

--- src/pages/dashbord/User/User.tsx
+++ src/pages/dashbord/User/User.tsx
@@ -1,59 +1,54 @@
+import {useEffect, useState} from "react";
 import Button from "../../../components/ui/button";
+import axios from "axios";
 
 const Users = () => {
-  const users = [
-    {
-      name: "John Doe",
-      email: "john.doe@example.com",
-      age: 30,
-      country: "USA",
-    },
-    {
-      name: "Alice Smith",
-      email: "alice.smith@example.com",
-      age: 25,
-      country: "Canada",
-    },
-    {
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      age: 28,
-      country: "UK",
-    },
-    {
-      name: "Eva Martinez",
-      email: "eva.martinez@example.com",
-      age: 35,
-      country: "Spain",
-    },
-    // Add more user objects as needed
-  ];
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get("/admin/user")
+      .then(({data}) => {
+        setUsers(data);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+        setIsLoading(false);
+      });
+  }, []);
 
   return (
     <div>
       <ul className="text-primary-500 flex flex-col justify-center items-center rounded-xl border ">
-        {users.map((user, idx) => (
-          <div
-            className="flex justify-between border w-3/4  p-4 gap-4 shadow-xl mt-5"
-            key={idx}>
-            <div>
-              <img
-                className="w-12 h-12 rounded-full"
-                src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg"
-                alt=""
-              />
+        {isLoading ? (
+          <h1 className="text-3xl">Loading...</h1>
+        ) : (
+          users.map((user, idx) => (
+            <div
+              className="flex justify-between border w-3/4  p-4 gap-4 shadow-xl mt-5"
+              key={idx}>
+              <div>
+                <img
+                  className="w-12 h-12 rounded-full"
+                  src={user.photoURL}
+                  alt=""
+                />
+              </div>
+              <div>
+                <p className="text-xl font-semibold">{user.name}</p>
+                <p className="text-xl font-semibold">{user.email}</p>
+              </div>
+              <div className=" ">
+                <Button className="me-2">Make Admin </Button>
+                <Button className="me-2">Make Mordaretor</Button>
+                <Button className="me-2">Delete</Button>
+              </div>
             </div>
-            <div>
-              <p className="text-xl font-semibold">{user.name}</p>
-              <p className="text-xl font-semibold">{user.email}</p>
-            </div>
-            <div className=" ">
-              <Button className="me-2">Make Admin </Button>
-              <Button className="me-2">Make Mordaretor</Button>
-              <Button className="me-2">Delete</Button>
-            </div>
-          </div>
-        ))}
+          ))
+        )}
       </ul>
     </div>
   );
