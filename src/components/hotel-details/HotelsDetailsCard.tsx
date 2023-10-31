a59492181704270e0817a61c9befc7f49e9177df
AbuBokorprog <abubokor1066@gmail.com>
2023-10-31 14:00:01 +0600

    hotelDetails layout ready

---
+++ src/components/hotel-details/HotelsDetailsCard.tsx
@@ -0,0 +1,46 @@
+import Button from "../ui/button";
+
+const HotelsDetailsCard = () => {
+  return (
+    <div>
+      <div className="max-w-sm bg-white border border-secondary-200 rounded-lg shadow dark:bg-secondary-800 dark:border-secondary-700">
+        <img
+          className="rounded-t-lg"
+          src="/docs/images/blog/image-1.jpg"
+          alt=""
+        />
+
+        <div className="p-5">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+
+          <p className="mb-3 font-normal text-secondary-700 dark:text-secondary-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <Button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+            Read more
+            <svg
+              className="w-3.5 h-3.5 ml-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HotelsDetailsCard;
