c727f0c2d3d544be7285e3f0f619a8ee38d1498a
AbuBokorprog <abubokor1066@gmail.com>
2023-10-31 13:53:36 +0600

    Admin rooms layout ready

--- src/pages/dashboard/admin/rooms/index.tsx
+++ src/pages/dashboard/admin/rooms/index.tsx
@@ -1,7 +1,44 @@
+// import { useEffect, useState } from "react";
+// import { axios } from "../../../../api";
+
+interface IFormInputs {
+  title: string;
+  thumbnails: Array<string>;
+  facilities: Array<string>;
+  capacity: {
+    children: number;
+    adult: number;
+  };
+  roomInfo: {
+    bedType: string;
+    view: number;
+    roomSize: string;
+    regularPrice: number;
+    discountedPrice: number;
+    additionalInfo: string;
+  };
+}
+
 const Rooms: React.FC = () => {
+  // const [loader, setLoader] = useState(false);
+  // const [room, setRooom] = useState<IFormInputs[]>();
+
+  // useEffect(() => {
+  //   axios
+  //     .get("admin/room")
+  //     .then(({ data }) => {
+  //       setRooom(data);
+  //       setLoader(true);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
+  // console.log(room);
   return (
     <div>
-      <h1>Rooms</h1>
+      <h2 className="text-center">Our Hotels</h2>
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4"></div>
     </div>
   );
 };
