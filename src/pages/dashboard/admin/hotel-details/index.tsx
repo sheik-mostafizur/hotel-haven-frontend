7d5e857cb61b610618a1a83d1cb7a7dc8de741e4
eshansaif <eshansaif1234@gmail.com>
2023-11-02 15:45:44 +0600

    hotal details and rooms page is done

--- src/pages/dashboard/admin/hotel-details/index.tsx
+++ src/pages/dashboard/admin/hotel-details/index.tsx
@@ -1,49 +1,25 @@
-// import {useEffect, useState} from "react";
-// import {useParams} from "react-router-dom";
-// import {axios} from "../../../../api";
-// import toastError from "../../../../utils/toast-error";
-// import {HashSpinner} from "../../../../components/spinner";
-
-// const HotelDetails = () => {
-//   const {_id} = useParams();
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [hotelDetails, setHotelDetails] = useState(null);
-
-//   useEffect(() => {
-//     setIsLoading(true);
-//     axios
-//       .get(`/admin/hotel/${_id}`)
-//       .then(({data}) => {
-//         setIsLoading(false);
-//         setHotelDetails(data);
-//       })
-//       .catch((error: any) => {
-//         toastError(error);
-//         setIsLoading(false);
-//       });
-//   }, []);
-
-//   return (
-//     <>{isLoading ? <HashSpinner /> : <>{JSON.stringify(hotelDetails)}</>}</>
-//   );
-// };
-
-// export default HotelDetails;
-
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { axios } from "../../../../api";
 import toastError from "../../../../utils/toast-error";
 import { HashSpinner } from "../../../../components/spinner";
 import Slider from "react-animated-slider";
 import "react-animated-slider/build/horizontal.css";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaChild, FaPeopleArrows } from "react-icons/fa";
+import { MdBedroomParent } from "react-icons/md";
+import { FcManager, FcMoneyTransfer } from "react-icons/fc";
+import { BiSolidOffer } from "react-icons/bi";
+import { HiInformationCircle } from "react-icons/hi2";
+import Button from "../../../../components/ui/button";
 
 const HotelDetails = () => {
   const { _id } = useParams();
   const [isLoading, setIsLoading] = useState(false);
   const [hotelDetails, setHotelDetails] = useState<any>(null);
 
-  console.log(hotelDetails?.room);
+  // console.log(hotelDetails?.room);
+  console.log(hotelDetails);
 
   useEffect(() => {
     setIsLoading(true);
@@ -60,18 +36,41 @@ const HotelDetails = () => {
   }, [_id]);
 
   return (
-    // <>{isLoading ? <HashSpinner /> : <>{JSON.stringify(hotelDetails)}</>}</>
     <>
       {isLoading ? (
         <HashSpinner />
       ) : hotelDetails ? (
         <div>
-          <h1>Hotel Details</h1>
-          <div className="grid md:grid-cols-3 gap-3">
+          <h1 className="text-center">Hotel Details & Rooms</h1>
+          <div>
+            <h3 className="font-bold my-3"> {hotelDetails?.hotel.name}</h3>
+            <h6 className="font-medium my-3 flex items-center gap-2">
+              <FcManager />{" "}
+              <span> Manager: {hotelDetails?.hotel.managerName}</span>
+            </h6>
+            <div className="flex items-center gap-6 mb-3">
+              <p className="flex items-center gap-2">
+                {" "}
+                <span className="flex gap-2 items-center">
+                  <FaLocationDot />{" "}
+                  <span>{hotelDetails?.hotel.address.location}</span>
+                </span>
+              </p>
+              <p className="flex items-center gap-2">
+                {" "}
+                <MdBedroomParent />{" "}
+                <span>
+                  Available Rooms: {hotelDetails?.hotel.availableRoom}
+                </span>
+              </p>
+            </div>
+          </div>
+
+          <div className="grid md:grid-cols-3 gap-3 justify-center items-center">
             {hotelDetails?.room.map((room: any, index: number) => (
               <div
                 key={index}
-                className=" w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                className=" w-full max-w-md bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
               >
                 <Slider autoplay={3000}>
                   {room.thumbnails.map((item: any, index: any) => (
@@ -84,82 +83,72 @@ const HotelDetails = () => {
                     >
                       <div className="center">
                         <h1>{item.title}</h1>
-                        {/* <p>{item.description}</p>
-                        <button>{item.button}</button> */}
                       </div>
                     </div>
                   ))}
                 </Slider>
-                <div className="px-5 pb-5">
-                  <a href="#">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                <div className="px-5 pb-5 gap-y-6">
+                  <div className="">
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white my-2">
                       {room.title}
                     </h5>
-                    <p>
-                      Capacity: Adult: {room.capacity.adult}, Child:{" "}
-                      {room.capacity.children}
+                    <p className="flex gap-3 mb-2">
+                      <span className="flex gap-1 justify-center items-center">
+                        {" "}
+                        <FaPeopleArrows />{" "}
+                        <span>Adult: {room.capacity.adult}</span>
+                      </span>
+                      <span className="flex gap-1 items-center justify-center">
+                        <FaChild />
+                        <span>Child: {room.capacity.children}</span>
+                      </span>
                     </p>
-                  </a>
-                  {/* <div className="flex items-center mt-2.5 mb-5">
-                    <svg
-                      className="w-4 h-4 text-yellow-300 mr-1"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 text-yellow-300 mr-1"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 text-yellow-300 mr-1"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 text-yellow-300 mr-1"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 text-gray-200 dark:text-gray-600"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-                      5.0
-                    </span>
-                  </div> */}
+                    <p className="mb-2">
+                      <span className="font-medium">
+                        {" "}
+                        <span className="flex items-center gap-2">
+                          <BiSolidOffer /> <span>Facilities:</span>
+                        </span>
+                      </span>
+                      <ul className="ms-8" style={{ listStyleType: "disc" }}>
+                        {room.facilities.map((facility: any, index: any) => (
+                          <li key={index}>{facility}</li>
+                        ))}
+                      </ul>
+                    </p>
+                    <div className="mb-2">
+                      <span className="flex items-center gap-2">
+                        <HiInformationCircle />{" "}
+                        <span className="font-medium">Other Information:</span>
+                      </span>
+                      <ul className="ms-8" style={{ listStyleType: "square" }}>
+                        <li> {room.roomInfo?.bedType}</li>
+                        <li> {room.roomInfo?.roomSize}</li>
+                        <li> {room.roomInfo?.view}</li>
+                        <li> {room.roomInfo?.additionalInfo}</li>
+                      </ul>
+                    </div>
+                  </div>
                   <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                      $599
+                    <span className=" text-gray-900 dark:text-white flex items-center justify-between gap-3">
+                      <span className="flex items-center gap-2">
+                        {" "}
+                        <FcMoneyTransfer />{" "}
+                        <span className="font-medium">Price:</span>
+                      </span>
+                      <span
+                        className="text-sm"
+                        style={{ textDecoration: "line-through" }}
+                      >
+                        BDT {room.roomInfo?.regularPrice}
+                      </span>
+                      <span>BDT {room.roomInfo?.discountedPrice}</span>
                     </span>
-                    <a
-                      href="#"
-                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
-                      Add to cart
-                    </a>
+                  </div>
+                  <div className="flex justify-end">
+                    <Link to="/admin/hotels">
+                      <Button size="sm">See All Hotels</Button>
+                    </Link>
                   </div>
                 </div>
               </div>
