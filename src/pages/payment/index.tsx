728e934d6af9deb0f51fbcd65ec441c75d472908
AbuBokorprog <abubokor1066@gmail.com>
2023-11-05 12:48:46 +0600

    navbar anf footer add on payment page

--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -1,52 +1,15 @@
 import { useParams } from "react-router-dom";
 import Container from "../../components/ui/container";
+import Main from "../../layout/main";
 
 const Payment = () => {
   const { _id } = useParams();
   console.log("RoomID: ", _id);
   return (
-    <Container>
-      <div className="py-4">
-        <div className="block w-full p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-          <p className="font-normal text-secondary-700 dark:text-secondary-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-        </div>
-      </div>
-      <div className="flex justify-center gap-4 mx-auto">
-        <div className="w-full">
-          <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-secondary-700 dark:text-secondary-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </div>
-          <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-secondary-700 dark:text-secondary-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </div>
-          <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-secondary-700 dark:text-secondary-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </div>
-          <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
+    <Main>
+      <Container>
+        <div className="py-4">
+          <div className="block w-full p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
               Noteworthy technology acquisitions 2021
             </h5>
@@ -56,49 +19,89 @@ const Payment = () => {
             </p>
           </div>
         </div>
-        <div className="max-w-sm h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <div>
-            <img
-              className="rounded-t-lg"
-              src="/docs/images/blog/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div className="p-5">
-            <a href="#">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <div className="flex justify-center gap-4 mx-auto">
+          <div className="w-full">
+            <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
                 Noteworthy technology acquisitions 2021
               </h5>
-            </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href="#"
-              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                className="w-3.5 h-3.5 ml-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
+              <p className="font-normal text-secondary-700 dark:text-secondary-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </div>
+            <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal text-secondary-700 dark:text-secondary-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </div>
+            <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal text-secondary-700 dark:text-secondary-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </div>
+            <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal text-secondary-700 dark:text-secondary-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
+          <div className="max-w-sm h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div>
+              <img
+                className="rounded-t-lg"
+                src="/docs/images/blog/image-1.jpg"
+                alt=""
+              />
+            </div>
+            <div className="p-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Noteworthy technology acquisitions 2021
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+              <a
+                href="#"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
+                Read more
+                <svg
+                  className="w-3.5 h-3.5 ml-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </Main>
   );
 };
 
