17f4027514385e1446ee3660b5f9a6af8cb65e6e
AbuBokorprog <abubokor1066@gmail.com>
2023-11-06 11:19:17 +0600

    payment page  improve

--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -1,63 +1,99 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Container from "../../components/ui/container";
 import Main from "../../layout/main";
+import { FaUserAlt, FaLock, FaCheckCircle } from "react-icons/fa";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import React from "react";
 
-const Payment = () => {
+interface IFormInputs {
+  fullName: string;
+  Email: string;
+  mobile: number;
+}
+
+const Payment: React.FC = () => {
   const { _id } = useParams();
   console.log("RoomID: ", _id);
+  const { handleSubmit, control, reset } = useForm<IFormInputs>({});
+  const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data);
   return (
     <Main>
-      <Container>
-        <div className="py-4">
-          <div className="block w-full p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-secondary-700 dark:text-secondary-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </div>
+      <Container className="px-8">
+        <div className="w-full">
+          <h3 className="">Room Title</h3>
         </div>
-        <div className="flex justify-center gap-4 mx-auto">
+        <div className="flex flex-col-reverse lg:flex-row justify-center py-2 items-start gap-4 mx-auto">
           <div className="w-full">
-            <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p className="font-normal text-secondary-700 dark:text-secondary-400">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order.
+            {/* <div className=" p-6 flex items-center gap-4 bg-white border border-secondary-200 rounded-lg shadow dark:bg-secondary-800 dark:border-secondary-700 ">
+              <span className="bg-primary-500 p-5 rounded-2xl">
+                <FaMoon></FaMoon>
+              </span>
+              <div>
+                <h6 className="">Collect 4 stamps with this stay</h6>
+                <p>10 stamps get you 1 reward night.</p>
+              </div>
+            </div> */}
+            {/* step 1 */}
+            <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
+              <div className="flex items-center gap-4">
+                <FaUserAlt></FaUserAlt>
+                <h5 className="">Step 1: Your details</h5>
+              </div>
+              <hr className="mt-1 border" />
+              <p className=" py-4 ">
+                <small>
+                  Please tell us the name of the guest staying at the hotel as
+                  it appears on the ID that they’ll present at check-in. If the
+                  guest has more than one last name, please enter them all.
+                </small>
               </p>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="fullName">Name</label>
+                <Controller
+                  name="fullName"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => <input {...field} />}
+                />
+                <br />
+                <label htmlFor="Email">Email</label>
+                <small>Your confirmation email goes here</small>
+                <Controller
+                  name="Email"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => <input {...field} />}
+                />
+                <br />
+                <label htmlFor="mobile">Mobile</label>
+                <small className="">
+                  We’ll only contact you in an emergency
+                </small>
+                <Controller
+                  name="mobile"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => <input {...field} type="number" />}
+                />
+              </form>
             </div>
-            <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p className="font-normal text-secondary-700 dark:text-secondary-400">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order.
+            {/* step 2 Payment Details*/}
+            <div className="block p-6 my-4 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
+              <div className="flex justify-between items-center">
+                <div className="flex items-center gap-4">
+                  <FaLock></FaLock>
+                  <h5 className="">Step 2: Payment details</h5>
+                </div>
+                <p>Your booking is safe and secure</p>
+              </div>
+              <hr className="mt-1 border" />
+              <p className="py-4 flex items-center gap-4 ">
+                <FaCheckCircle></FaCheckCircle> We never charge any card fees
               </p>
-            </div>
-            <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p className="font-normal text-secondary-700 dark:text-secondary-400">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order.
-              </p>
-            </div>
-            <div className="block p-6 bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p className="font-normal text-secondary-700 dark:text-secondary-400">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order.
-              </p>
+              {/* Titumar bhai Your task is start here */}
             </div>
           </div>
+          {/* Room Details */}
           <div className="max-w-sm h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <div>
               <img
