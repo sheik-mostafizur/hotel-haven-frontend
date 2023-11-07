4b67f86a2b4bee3a399a352b05c67b3d6e32647e
eshansaif <eshansaif1234@gmail.com>
2023-11-07 12:56:56 +0600

    active navigation on selected menu is added for dashbaord

--- src/pages/dashboard/dashboard-nav/DashboardNavAdmin.tsx
+++ src/pages/dashboard/dashboard-nav/DashboardNavAdmin.tsx
@@ -1,14 +1,21 @@
-import {FaUsers} from "react-icons/fa";
-import {RiHotelFill} from "react-icons/ri";
-import {Link} from "react-router-dom";
+import { FaUsers } from "react-icons/fa";
+import { RiHotelFill } from "react-icons/ri";
+import { Link, useLocation } from "react-router-dom";
 
 const DashboardNavAdmin = () => {
+  const location = useLocation();
+
   return (
     <>
       <li>
         <Link
           to="/admin/users"
-          className="flex items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover:bg-secondary-700 group">
+          className={`flex items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover-bg-secondary-700 group ${
+            location.pathname === "/admin/users"
+              ? "text-primary-500 bg-primary-400 dark:bg-primary-700"
+              : ""
+          }`}
+        >
           <FaUsers className="flex-shrink-0 w-5 h-5 text-secondary-500 transition duration-75 dark:text-secondary-400 group-hover:text-secondary-900 dark:group-hover:text-white" />
           <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
         </Link>
@@ -16,7 +23,12 @@ const DashboardNavAdmin = () => {
       <li>
         <Link
           to="/admin/hotels"
-          className="flex items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover:bg-secondary-700 group">
+          className={`flex items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover-bg-secondary-700 group ${
+            location.pathname === "/admin/hotels"
+              ? "text-primary-500 bg-primary-400 dark:bg-primary-700"
+              : ""
+          }`}
+        >
           <RiHotelFill className="flex-shrink-0 w-5 h-5 text-secondary-500 transition duration-75 dark:text-secondary-400 group-hover:text-secondary-900 dark:group-hover:text-white" />
           <span className="flex-1 ml-3 whitespace-nowrap">Hotels</span>
         </Link>
--- src/pages/dashboard/dashboard-nav/DashboardNavCustomer.tsx
+++ src/pages/dashboard/dashboard-nav/DashboardNavCustomer.tsx
@@ -1,14 +1,21 @@
-import {BsFillBox2HeartFill} from "react-icons/bs";
-import {Link} from "react-router-dom";
+import { BsFillBox2HeartFill } from "react-icons/bs";
+import { Link, useLocation } from "react-router-dom";
 
 const DashboardNavCustomer = () => {
+  const location = useLocation();
+
   return (
     <>
       <li>
         <Link
           to="wishlist"
-          className="flex items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover:bg-secondary-700 group">
-          <BsFillBox2HeartFill className="flex-shrink-0 w-5 h-5 text-secondary-500 transition duration-75 dark:text-secondary-400 group-hover:text-secondary-900 dark:group-hover:text-white" />
+          className={`flex items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover-bg-secondary-700 group ${
+            location.pathname === "/dashboard/wishlist"
+              ? "text-primary-500 bg-primary-400 dark:bg-primary-700"
+              : ""
+          }`}
+        >
+          <BsFillBox2HeartFill className="flex-shrink-0 w-5 h-5 text-secondary-500 transition duration-75 dark:text-secondary-400 group-hover:text-secondary-900 dark:group-hover-text-white" />
           <span className="flex-1 ml-3 whitespace-nowrap">Wishlist</span>
         </Link>
       </li>
