c5b8dee772f435dc21945e6b3d12446cbdf4c033
AbuBokorprog <abubokor1066@gmail.com>
2023-11-07 14:15:45 +0600

    view hotels image improve(grid)

--- src/pages/hotel/hotel-details/index.tsx
+++ src/pages/hotel/hotel-details/index.tsx
@@ -1,13 +1,13 @@
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Main from "../../../layout/main";
-import {HashSpinner} from "../../../components/spinner";
+import { HashSpinner } from "../../../components/spinner";
 import Container from "../../../components/ui/container";
 import GoogleMapReact from "google-map-react";
-import {useGetHotelByIdQuery} from "../../../api/public-api";
-import {useAppSelector} from "../../../redux/hooks";
+import { useGetHotelByIdQuery } from "../../../api/public-api";
+import { useAppSelector } from "../../../redux/hooks";
 import CardRoom from "./CardRoom";
 
-const AnyReactComponent = ({text}) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 interface HotelDetails {
   hotel: {
     photoURL: string;
@@ -27,19 +27,19 @@ interface HotelDetails {
     thumbnails: string[];
     title: string;
     facilities: string[];
-    roomInfo: {[key: string]: string};
+    roomInfo: { [key: string]: string };
   }[];
 }
 
 const HotelDetails: React.FC = () => {
   const hotelFilter = useAppSelector((state) => state.hotelFilter);
-  const {_id} = useParams();
-  const {data: viewHotels, isLoading} = useGetHotelByIdQuery({
+  const { _id } = useParams();
+  const { data: viewHotels, isLoading } = useGetHotelByIdQuery({
     _id,
     params: hotelFilter,
   });
 
-  const {hotel} = viewHotels || [];
+  const { hotel, rooms } = viewHotels || [];
 
   const defaultProps = {
     center: {
@@ -48,6 +48,8 @@ const HotelDetails: React.FC = () => {
     },
     zoom: 11,
   };
+
+  // console.log(rooms[0].thumbnails[0]);
 
   return (
     <Main>
@@ -59,11 +61,69 @@ const HotelDetails: React.FC = () => {
             {viewHotels ? (
               <>
                 <div className="bg-white border border-secondary-200 rounded-lg shadow dark:bg-secondary-800 dark:border-secondary-700">
-                  <img
-                    className="rounded-t-lg h-96 w-full"
-                    src={hotel?.photoURL}
-                    alt=""
-                  />
+                  <div className="grid grid-cols-2 items-center gap-1">
+                    <div>
+                      <img
+                        className="rounded-t-lg h-96 w-full"
+                        src={hotel?.photoURL}
+                        alt="hotel image"
+                      />
+                    </div>
+                    <div className="grid grid-cols-2 gap-1">
+                      {rooms[0]?.thumbnails[1] ? (
+                        <img
+                          className="rounded-t-lg h-48 w-full"
+                          src={rooms[0]?.thumbnails[1]}
+                          alt="Room image"
+                        />
+                      ) : (
+                        <img
+                          className="rounded-t-lg h-48 w-full"
+                          src="https://media.istockphoto.com/id/1213695547/photo/3d-rendering-of-an-elegant-bedroom.jpg?s=612x612&w=0&k=20&c=yxOoaz2IAd9zvtlXeS-Th-AiXhaCtMIxOONfGbtGeG8="
+                          alt="Room image"
+                        />
+                      )}
+                      {rooms[1]?.thumbnails[1] ? (
+                        <img
+                          className="rounded-t-lg h-48 w-full"
+                          src={rooms[1]?.thumbnails[1]}
+                          alt="room image"
+                        />
+                      ) : (
+                        <img
+                          className="rounded-t-lg h-48 w-full"
+                          src="https://images.adsttc.com/media/images/622a/1b96/620b/c901/65c4/d5ff/newsletter/cama-arquitetura-06b.jpg?1646926752"
+                          alt="room image"
+                        />
+                      )}
+                      {rooms[0]?.thumbnails[2] ? (
+                        <img
+                          className="rounded-b-lg h-48 w-full"
+                          src={rooms[0].thumbnails[2]}
+                          alt="room image"
+                        />
+                      ) : (
+                        <img
+                          className="rounded-b-lg h-48 w-full"
+                          src="https://media.designcafe.com/wp-content/uploads/2022/07/29185240/industrial-rustic-living-room-in-earthy-tones.jpg"
+                          alt="room image"
+                        />
+                      )}
+                      {rooms[1]?.thumbnails[2] ? (
+                        <img
+                          className="rounded-b-lg h-48 w-full"
+                          src={rooms[1]?.thumbnails[2]}
+                          alt="room image"
+                        />
+                      ) : (
+                        <img
+                          className="rounded-b-lg h-48 w-full"
+                          src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsb2ZmaWNlNF9waG90b19vZl9hX2ZyYW1lX2luX3RoZV9saXZpbmdfcm9vbV9pbl90aGVfc3R5bF85YWM1MjY1ZS02OTdjLTQ4OWMtYTFmYS03NzgzMjJlMTEwODNfMi5qcGc.jpg"
+                          alt="Room image"
+                        />
+                      )}
+                    </div>
+                  </div>
                   <div className="p-5">
                     <div>
                       <h5 className="mb-2 text-2xl font-bold tracking-tight text-secondary-900 dark:text-white">
@@ -93,11 +153,15 @@ const HotelDetails: React.FC = () => {
                     <p>No rooms available</p>
                   )}
                 </div>
-                <div className="my-8" style={{height: "500px", width: "100%"}}>
+                <div
+                  className="my-8"
+                  style={{ height: "500px", width: "100%" }}
+                >
                   <GoogleMapReact
-                    bootstrapURLKeys={{key: ""}}
+                    bootstrapURLKeys={{ key: "" }}
                     defaultCenter={defaultProps.center}
-                    defaultZoom={defaultProps.zoom}>
+                    defaultZoom={defaultProps.zoom}
+                  >
                     <AnyReactComponent
                       lat={hotel?.address?.map?.lat}
                       lng={hotel?.address?.map?.lng}
