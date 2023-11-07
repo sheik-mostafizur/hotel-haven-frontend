35b35ce32ff7b892da638340e18c027dca30040d
AbuBokorprog <abubokor1066@gmail.com>
2023-11-07 16:24:54 +0600

    hotel details page add fees & policies

--- src/pages/hotel/hotel-details/index.tsx
+++ src/pages/hotel/hotel-details/index.tsx
@@ -153,8 +153,88 @@ const HotelDetails: React.FC = () => {
                     <p>No rooms available</p>
                   )}
                 </div>
+
+                <div className=" p-6 my-6 grid lg:grid-cols-3 gap-2  bg-white border border-secondary-200 rounded-lg shadow hover:bg-secondary-100 dark:bg-secondary-800 dark:border-secondary-700 dark:hover:bg-secondary-700">
+                  <div className="w-52">
+                    <h3 className="my-2">Fees & policies</h3>
+                  </div>
+                  <div className="w-full col-span-2">
+                    <div className="">
+                      <h5>Optional extras</h5>
+                      <li>
+                        <span className="font-medium">Buffet breakfast</span> is
+                        offered for an extra charge of approximately BDT 2200
+                        per person.
+                      </li>
+                      <li>
+                        <span className="font-medium">Airport</span> shuttle
+                        service is offered for an extra charge of BDT 2000 per
+                        room (one-way)
+                      </li>
+                    </div>
+                    <div className="my-8">
+                      <h5>Children & extra beds</h5>
+                      <li>
+                        Supervised childcare is available for an extra charge
+                      </li>
+                      <li>
+                        Cribs/infant beds are available for BDT 25 per night
+                      </li>
+                      <li>
+                        Airport shuttle for children between the ages of 2 and
+                        12 costs BDT 2000 (one-way)
+                      </li>
+                    </div>
+                    <div className="my-8">
+                      <h5>Pool, spa, & gym (if applicable)</h5>
+                      <li>Pool access available from 8:00 AM to 6:00 PM</li>
+                      <li>
+                        Reservations are required for spa treatments and can be
+                        made by contacting the property before arrival at the
+                        number on the booking confirmation
+                      </li>
+                    </div>
+                    <div className="my-8">
+                      <h5>Hygiene & cleanliness</h5>
+                      <p>
+                        <small>
+                          This property advises that enhanced cleaning and guest
+                          safety measures are currently in place. <br /> <br />
+                          Disinfectant is used to clean the property;
+                          commonly-touched surfaces are cleaned with
+                          disinfectant between stays; bed sheets and towels are
+                          laundered at a temperature of at least 60°C/140°F.{" "}
+                          <br /> <br /> Social distancing measures are in place;
+                          staff at the property wear personal protective
+                          equipment; guests are provided with hand sanitizer.{" "}
+                          <br /> <br /> Contactless check-in and contactless
+                          check-out are available. <br /> <br /> Each guestroom
+                          is kept vacant for a minimum of 72 hours between
+                          bookings.
+                        </small>
+                      </p>
+                    </div>
+                    <div className="my-8">
+                      <h5>Policies</h5>
+                      <p>
+                        <small>
+                          The property has connecting/adjoining rooms, which are
+                          subject to availability and can be requested by
+                          contacting the property using the number on the
+                          booking confirmation.
+                          <br /> Guests can rest easy knowing there's a fire
+                          extinguisher, a smoke detector, and a first aid kit on
+                          site. <br /> This property accepts credit cards and
+                          cash. <br /> Please note that cultural norms and guest
+                          policies may differ by country and by property. The
+                          policies listed are provided by the property.
+                        </small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
                 <div
-                  className="my-8"
+                  className="my-6 bg-white border border-secondary-200 rounded-lg shadow dark:bg-secondary-800 dark:border-secondary-700"
                   style={{ height: "500px", width: "100%" }}
                 >
                   <GoogleMapReact
