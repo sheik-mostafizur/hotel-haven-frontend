54ac5fb5955afb5f5c89943c40caf34964c7dbb7
SiFaisal <sifaisalcox@gmail.com>
2023-11-09 01:38:24 +0600

    receive rating number and comment from user with handlar and responsive

--- src/components/RatingPopUp/index.tsx
+++ src/components/RatingPopUp/index.tsx
@@ -1,69 +1,81 @@
-import { useState } from "react";
+import { useState ,  ChangeEvent } from "react";
 
-
-const RatingPopUp = () => {
-  const [message, setMessage] = useState('');
+const RatingPopUp : React.FC = () => {
+  const [rating, setRating] = useState<number>(0);
+  const [message, setMessage] = useState<string>('');
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState<boolean>(true);
 
   const handleRatingSubmit = () => {
-    // Handle the rating submission here, e.g., send data to a server
-    console.log('Rating submitted with message:', message);
+    console.log('Rating submitted:', rating);
+    console.log('Message submitted:', message);
+    setRating(0);
+    setMessage('');
+    setIsSubmitDisabled(true);
+  };
+
+  const handleRatingChange = (newRating : number) => {
+    setRating(newRating);
+    setIsSubmitDisabled(newRating === 0 );
+  };
+
+  const handleMessageChange = (e : ChangeEvent<HTMLTextAreaElement>) => {
+    const newMessage : string  =  e.target.value;
+    setMessage(newMessage);
+    setIsSubmitDisabled(rating === 0 );
   };
 
   return (
-    <div className="min-h-screen bg-gray-300 py-6 flex flex-col justify-center sm:py-12">
-      <div className="py-3 sm:max-w-xl sm:mx-auto">
-        <div className="bg-white min-w-1xl flex flex-col rounded-xl shadow-lg">
-          <div className="px-12 py-5">
-            <h2 className="text-gray-800 text-3xl font-semibold">Your opinion matters to us!</h2>
-          </div>
-          <div className="bg-gray-200 w-full flex flex-col items-center">
-            <div className="flex flex-col items-center py-6 space-y-3">
-              <span className="text-lg text-gray-800">How was the quality of the call?</span>
-              <div className="flex space-x-3">
-                {[1, 2, 3, 4, 5].map((index) => (
-                  <svg
-                    key={index}
-                    className="w-12 h-12 text-yellow-500"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                  </svg>
-                ))}
-              </div>
-            </div>
-            <div className="w-3/4 flex flex-col">
-              <textarea
-                rows={3}
-                className="p-4 text-gray-500 rounded-xl resize-none"
-                placeholder="Leave a message, if you want"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              ></textarea>
-              <button
-                onClick={handleRatingSubmit}
-                className="py-3 my-8 text-lg bg-gradient-to-r from-purple-500 to-indigo-600 rounded-xl text-white"
-              >
-                Rate now
-              </button>
+    <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
+    <div className="py-3 sm:max-w-xl sm:mx-auto">
+      <div className="bg-white min-w-1xl border-2 flex flex-col rounded-xl shadow-lg">
+        <div className="px-4 lg:px-12 py-5">
+          <h2 className="text-gray-800 text-xl lg:text-3xl font-semibold">Share your satisfaction!!</h2>
+        </div>
+        <div className="bg-gray-200 w-full flex flex-col items-center">
+          <div className="flex flex-col items-center py-6 space-y-3">
+            <span className="text-2xl font-semibold text-gray-800">Rating Us</span>
+            <div className="flex items-center space-x-3">
+              {[1, 2, 3, 4, 5].map((index) => (
+                <svg
+                  key={index}
+                  className={`w-8 h-8 ${rating >= index ? 'text-yellow-400' : 'text-gray-300 dark:text-gray-500'}`}
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                  onClick={() => handleRatingChange(index)}
+                  style={{ cursor: 'pointer' }}
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+              ))}
             </div>
           </div>
-          <div className="h-20 flex items-center justify-center">
-            <a href="#" className="text-gray-600">
-              Maybe later
-            </a>
+          <div className="w-3/4 flex flex-col">
+            <textarea
+              rows={3}
+              className="p-4 text-gray-500 rounded-xl"
+              placeholder="Leave a message, if you want"
+              value={message}
+              onChange={handleMessageChange}
+            ></textarea>
+            <button
+              onClick={handleRatingSubmit}
+              className={`py-3 my-8 text-lg bg-gradient-to-r from-purple-500 to-indigo-600 rounded-xl text-white ${isSubmitDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+              disabled={isSubmitDisabled}
+            >
+              Rate now
+            </button>
           </div>
         </div>
-
-        <div className="mt-8 text-gray-700">
-          Crédits{' '}
-          <a className="font-bold" href="https://dribbble.com/shots/12052834-Rating-popup">
-            Goga
+        <div className="h-20 flex items-center justify-center">
+          <a href="#" className="text-gray-600">
+            Maybe later
           </a>
         </div>
       </div>
     </div>
+  </div>
   );
 };
 
