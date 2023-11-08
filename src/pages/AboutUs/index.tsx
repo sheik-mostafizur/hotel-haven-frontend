cc825dab7f711452cc539beb2e135104f6cfc87c
SiFaisal <sifaisalcox@gmail.com>
2023-11-07 20:09:27 -0800

    resolved about us page

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -7,16 +7,9 @@ const AboutUs: React.FC = () => {
         <div className="py-12 bg-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="lg:text-center">
-              <h2 className="font-heading mb-4 bg-orange-100 text-orange-800 px-4 py-2 rounded-lg md:w-64 md:mx-auto text-xs font-semibold tracking-widest text-black uppercase title-font">
+              <h2 className="font-heading mb-4 bg-orange-100 text-orange-800 px-6 py-4 rounded-lg md:w-1/2 md:mx-auto text-center text-xl md:text-4xl font-semibold tracking-widest text-black uppercase title-font">
                 Why choose us?
               </h2>
-              <p className="font-heading mt-2 text-3xl leading-8 font-semibold tracking-tight text-gray-900 sm:text-4xl">
-                We know tech, we know finance. We are fintech experts.
-              </p>
-              <p className="mt-4 max-w-2xl text-lg text-gray-500 lg:mx-auto">
-                We know how to handle taxation for all the countries we operate
-                in. We care for our customers and help them manage cashflows.
-              </p>
             </div>
 
             <div className="mt-10">
@@ -30,13 +23,11 @@ const AboutUs: React.FC = () => {
                       />
                     </div>
                     <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">
-                      Powerful API
+                    Unparalleled Luxury and Comfort
                     </p>
                   </dt>
                   <dd className="mt-2 ml-16 text-base text-gray-500">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
+                   Our home is not just a place to rest your head; it's an experience in luxury and comfort. Our [Number of Rooms] beautifully appointed rooms and suites are designed with your relaxation and convenience in mind. From plush bedding to stunning views, we provide the perfect environment for your enjoyment
                   </dd>
                 </div>
                 <div className="relative">
@@ -48,13 +39,11 @@ const AboutUs: React.FC = () => {
                       />
                     </div>
                     <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">
-                      Easy to integrate SDK
+                     Impeccable Service
                     </p>
                   </dt>
                   <dd className="mt-2 ml-16 text-base text-gray-500">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
+                  Our dedicated staff is committed to providing service that exceeds your expectations. From the moment you arrive until your departure, you'll be greeted with warm smiles and a willingness to cater to your every need. Your comfort and satisfaction are our top priorities.
                   </dd>
                 </div>
                 <div className="relative">
@@ -66,13 +55,13 @@ const AboutUs: React.FC = () => {
                       />
                     </div>
                     <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">
-                      Low Transaction Cost
+                    Exclusive Offers
                     </p>
                   </dt>
                   <dd className="mt-2 ml-16 text-base text-gray-500">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
+                  In summary, at [Your Hotel's Name], we're not just a place to stay; we're your gateway to a world of luxury, comfort, and unforgettable moments. Choose us for an experience that's truly exceptional.
+
+Thank you for considering us as your preferred destination. We look forward to the opportunity to host you and ensure your stay is nothing short of extraordinary.
                   </dd>
                 </div>
                 <div className="relative">
@@ -84,13 +73,11 @@ const AboutUs: React.FC = () => {
                       />
                     </div>
                     <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">
-                      Powerful Dashboard
+                    Commitment to Sustainability
                     </p>
                   </dt>
                   <dd className="mt-2 ml-16 text-base text-gray-500">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
+                  We care about the planet, and we're taking steps to reduce our environmental footprint. When you choose [Your Hotel's Name], you're choosing a hotel with a commitment to sustainability and responsible practices.
                   </dd>
                 </div>
               </dl>
@@ -102,22 +89,21 @@ const AboutUs: React.FC = () => {
       <section className="py-12 md:py-24">
         <div className="w-full max-w-6xl mx-auto px-4 sm:px-6 md:px-8 relative">
           <div className="shadow rounded-xl">
-            <div className="grid overflow-hidden text-white shadow-xl md:grid-cols-2 bg-blue-600 rounded-xl">
+            <div className="grid overflow-hidden text-white shadow-xl md:grid-cols-2 bg-[#5355] rounded-xl">
               <aside className="p-8 space-y-4 md:p-16">
                 <h2 className="text-2xl font-bold tracking-tight md:text-4xl font-headline">
-                  Ready to dive in? Start your free trial today.
+                  Ready to enjoy your days. Let's start to booking our awesome hotels 
                 </h2>
 
                 <p className="font-medium text-blue-100 md:text-2xl">
-                  Create an account to enjoy your 5-day free trial, no credit
-                  card required.
+                  Explore our site, know our services and booked which you will be comfort.
                 </p>
 
                 <div>
                   <a
-                    href="#"
+                    href="/hotel"
                     className="bg-white text-blue-600 px-4 py-2 mt-3 rounded-xl">
-                    Start free trial
+                    let's go to hotels
                   </a>
                 </div>
               </aside>
@@ -125,7 +111,7 @@ const AboutUs: React.FC = () => {
               <aside className="relative hidden md:block">
                 <img
                   className="absolute inset-0 object-cover object-left-top w-full h-full mt-16 -mr-16 rounded-tl-lg"
-                  src="https://picsum.photos/200"
+                  src="https://img.freepik.com/free-photo/modern-studio-apartment-design-with-bedroom-living-space_1262-12375.jpg"
                   alt="Discover our beautiful panel"
                 />
               </aside>
@@ -135,27 +121,25 @@ const AboutUs: React.FC = () => {
       </section>
 
       <div className="text-center p-8">
-        <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
-          Why to choose US?
+        <h2 className="font-bold text-orange-800 text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+          Our Awesome Services
         </h2>
 
         {/* First content section */}
         <div className="flex flex-wrap items-center mt-20 text-left text-center">
-          <div className="w-full md:w-3/5 lg:w-1/2 px-4">
+          <div className="w-full md:w-3/5 lg:w-1/2 px-2">
             <img
-              src="https://via.placeholder.com/400x240"
+              src="https://img.freepik.com/free-photo/hammocks-with-palm-trees_1203-201.jpg?size=432"
               alt="gem"
               className="inline-block rounded shadow-lg border border-merino-400"
             />
           </div>
           <div className="w-full md:w-2/5 lg:w-1/2 px-4 text-center md:text-left lg:pl-12">
             <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-              Simple integration
+            Luxurious Accommodations
             </h3>
             <p className="sm:text-lg mt-6">
-              Use the LocaleData gem to download translations directly into your
-              Ruby on Rails projects using the provided command line interface.
-              Just create a project and follow the step-by-step instructions.
+            Ensure that your rooms and suites are comfortable, well-furnished, and equipped with modern amenities.
             </p>
           </div>
         </div>
@@ -164,63 +148,65 @@ const AboutUs: React.FC = () => {
         <div className="flex flex-wrap items-center mt-20 text-left text-center">
           <div className="w-full md:w-3/5 lg:w-1/2 px-4">
             <img
-              src="https://via.placeholder.com/400x240"
+              src="https://img.freepik.com/free-photo/woman-talking-with-hotel-receptionist-lobby_23-2149304051.jpg?size=432"
               alt="project members"
               className="inline-block rounded shadow-lg border border-merino-400"
             />
           </div>
           <div className="w-full md:w-2/5 lg:w-1/2 px-4 md:order-first text-center md:text-left lg:pr-12">
             <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-              Easy collaboration
+            Gourmet Dining
             </h3>
             <p className="sm:text-lg mt-6">
-              All LocaleData projects are private. Each project can have
-              multiple collaborators with different roles and access
-              permissions. You determine who can see and edit your translations.
-              Just add admins, developers, translators and configure their
-              access rights.
+            Offer on-site restaurants and bars with a diverse menu, special cuisines, and a selection of beverages.
             </p>
           </div>
         </div>
 
-        {/* Additional content sections (repeat the structure for other sections) */}
+        {/* thirth content section.... */}
+
+        <div className="flex flex-wrap items-center mt-20 text-left text-center">
+          <div className="w-full md:w-3/5 lg:w-1/2 px-4">
+            <img
+              src="https://img.freepik.com/free-photo/close-up-beautiful-african-woman-enjoying-massage-spa-salon_176420-13959.jpg?size=432"
+              alt="gem"
+              className="inline-block rounded shadow-lg border border-merino-400"
+            />
+          </div>
+          <div className="w-full md:w-2/5 lg:w-1/2 px-4 text-center md:text-left lg:pl-12">
+            <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
+            Spa and Wellness Cente
+            </h3>
+            <p className="sm:text-lg mt-6">
+            Provide a spa and wellness facility for relaxation, massages, and beauty treatments.
+            </p>
+          </div>
+        </div>
+
+        {/* fourth content section */}
+        <div className="flex flex-wrap items-center mt-20 text-left text-center">
+          <div className="w-full md:w-3/5 lg:w-1/2 px-4">
+            <img
+              src="https://img.freepik.com/free-photo/team-business-managers-working-with-new-startup-project_171337-5864.jpg?size=432"
+              alt="project members"
+              className="inline-block rounded shadow-lg border border-merino-400"
+            />
+          </div>
+          <div className="w-full md:w-2/5 lg:w-1/2 px-4 md:order-first text-center md:text-left lg:pr-12">
+            <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
+            Meetings and Events
+            </h3>
+            <p className="sm:text-lg mt-6">
+            Offer flexible event spaces for business meetings, conferences, weddings, and other special occasions.
+            </p>
+          </div>
+        </div>
+
+
+
       </div>
 
-      <div className="max-w-xl mx-auto mt-16 flex w-full flex-col border rounded-lg bg-white p-8">
-        <h2 className="title-font mb-1 text-lg font-medium text-gray-900">
-          Feedback
-        </h2>
-        <p className="mb-5 leading-relaxed text-gray-600">
-          If you had any issues or you liked our product, please share with us!
-        </p>
-        <div className="mb-4">
-          <label htmlFor="email" className="text-sm leading-7 text-gray-600">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="w-full rounded border border-gray-300 bg-white py-1 px-3 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="message" className="text-sm leading-7 text-gray-600">
-            Message
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            className="h-32 w-full resize-none rounded border border-gray-300 bg-white py-1 px-3 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-          />
-        </div>
-        <button className="rounded border-0 bg-indigo-500 py-2 px-6 text-lg text-white hover:bg-indigo-600 focus:outline-none">
-          Send
-        </button>
-        <p className="mt-3 text-xs text-gray-500">
-          Feel free to connect with us on social media platforms.
-        </p>
-      </div>
+      
     </Main>
   );
 };
