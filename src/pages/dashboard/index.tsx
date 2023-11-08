167d8512973b61a2d4788635de4eb2b6d5e9574a
AbuBokorprog <abubokor1066@gmail.com>
2023-11-08 13:05:14 +0600

    Home Button added in all users dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,18 +1,18 @@
-import {Link, Outlet, useLocation} from "react-router-dom";
-import {useAppDispatch, useAppSelector} from "../../redux/hooks";
+import { Link, Outlet, useLocation } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import {
   DashboardNavAdmin,
   DashboardNavCustomer,
   DashboardNavManager,
 } from "./dashboard-nav";
-import {BiLogoBlogger, BiSolidDashboard} from "react-icons/bi";
-import {FaSignOutAlt} from "react-icons/fa";
-import {AiOutlineAlignLeft} from "react-icons/ai";
-import {CgProfile} from "react-icons/cg";
+import { BiLogoBlogger, BiSolidDashboard } from "react-icons/bi";
+import { FaSignOutAlt, FaHome } from "react-icons/fa";
+import { AiOutlineAlignLeft } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
 import ROLE from "../../constants/ROLE";
-import {logout} from "../../redux/authSlice";
+import { logout } from "../../redux/authSlice";
 import swal from "sweetalert";
-import {useState} from "react";
+import { useState } from "react";
 
 const Dashboard = () => {
   const user = useAppSelector((state) => state.auth.user);
@@ -41,7 +41,8 @@ const Dashboard = () => {
         type="button"
         className={`${
           toggleMenu ? "block ml-auto" : "ml-3 inline-flex"
-        } items-center p-2 mt-2 text-sm text-secondary-500 rounded-lg sm:hidden hover:bg-secondary-100 focus:outline-none focus:ring-2 focus:ring-secondary-200 dark:text-secondary-400 dark:hover:bg-secondary-700 dark:focus:ring-secondary-600`}>
+        } items-center p-2 mt-2 text-sm text-secondary-500 rounded-lg sm:hidden hover:bg-secondary-100 focus:outline-none focus:ring-2 focus:ring-secondary-200 dark:text-secondary-400 dark:hover:bg-secondary-700 dark:focus:ring-secondary-600`}
+      >
         <span className="sr-only">Open sidebar</span>
         <AiOutlineAlignLeft className="w-6 h-6" />
       </button>
@@ -51,7 +52,8 @@ const Dashboard = () => {
         className={`${
           toggleMenu ? "" : "-translate-x-full"
         } fixed top-0 left-0 z-40 w-64 h-screen transition-transform sm:translate-x-0`}
-        aria-label="Sidebar">
+        aria-label="Sidebar"
+      >
         <div className="h-full px-3 py-4 overflow-y-auto bg-secondary-50 dark:bg-secondary-800">
           <div className="flex flex-col items-center pl-2.5 mb-5">
             <div className="relative">
@@ -85,7 +87,8 @@ const Dashboard = () => {
                     location.pathname === "/dashboard"
                       ? "text-primary-500 bg-primary-400 dark:bg-primary-700"
                       : ""
-                  }`}>
+                  }`}
+                >
                   <BiSolidDashboard className="flex-shrink-0 w-5 h-5 text-secondary-500 transition duration-75 dark:text-secondary-400 group-hover:text-secondary-900 dark:group-hover:text-white" />
                   <span className="ml-3">Dashboard</span>
                 </Link>
@@ -98,7 +101,8 @@ const Dashboard = () => {
             <li>
               <Link
                 to={"profile"}
-                className="flex cursor-pointer items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover-bg-secondary-700 group">
+                className="flex cursor-pointer items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover-bg-secondary-700 group"
+              >
                 <CgProfile className="flex-shrink-0 w-5 h-5 text-secondary-500 transition duration-75 dark:text-secondary-400 group-hover:text-secondary-900 dark:group-hover:text-white" />
                 <span className="flex-1 ml-3 whitespace-nowrap">Profile</span>
               </Link>
@@ -106,15 +110,26 @@ const Dashboard = () => {
             <li>
               <Link
                 to={"blogs"}
-                className="flex cursor-pointer items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover-bg-secondary-700 group">
+                className="flex cursor-pointer items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover-bg-secondary-700 group"
+              >
                 <BiLogoBlogger className="flex-shrink-0 w-5 h-5 text-secondary-500 transition duration-75 dark:text-secondary-400 group-hover:text-secondary-900 dark:group-hover:text-white" />
                 <span className="flex-1 ml-3 whitespace-nowrap">Blogs</span>
               </Link>
             </li>
             <li>
+              <Link
+                to={"/"}
+                className="flex cursor-pointer items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover-bg-secondary-700 group"
+              >
+                <FaHome className="flex-shrink-0 w-5 h-5 text-secondary-500 transition duration-75 dark:text-secondary-400 group-hover:text-secondary-900 dark:group-hover:text-white" />
+                <span className="flex-1 ml-3 whitespace-nowrap">Home</span>
+              </Link>
+            </li>
+            <li>
               <a
                 onClick={handleSignOut}
-                className="flex cursor-pointer items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover-bg-secondary-700 group">
+                className="flex cursor-pointer items-center p-2 text-secondary-900 rounded-lg dark:text-white hover:bg-secondary-100 dark:hover-bg-secondary-700 group"
+              >
                 <FaSignOutAlt className="flex-shrink-0 w-5 h-5 text-secondary-500 transition duration-75 dark:text-secondary-400 group-hover:text-secondary-900 dark:group-hover:text-white" />
                 <span className="flex-1 ml-3 whitespace-nowrap">Sign Out</span>
               </a>
