59524b9240f52924fa4e6216926ad033294bcea9
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-11-09 17:17:48 +0600

    fix single blog

--- src/pages/blogs/SingleBlog.tsx
+++ src/pages/blogs/SingleBlog.tsx
@@ -1,10 +1,9 @@
-import React, {useEffect, useState} from "react";
 import {Link, useParams} from "react-router-dom";
-import fetchData from "../../hooks/fetch-data";
 import Container from "../../components/ui/container";
 import Main from "../../layout/main";
 import {FcLikePlaceholder} from "react-icons/fc";
 import {useGetBlogByIdQuery} from "../../api/private-api";
+import {HashSpinner} from "../../components/spinner";
 
 interface SingleBlog {
   _id: number;
@@ -14,75 +13,68 @@ interface SingleBlog {
   userName: string;
   userProfile: string;
   publishDate: string;
-  likes: number;
+  likeCount: number;
 }
 
 const SingleBlog: React.FC = () => {
   const {_id} = useParams();
-  const {data, isLoading} = useGetBlogByIdQuery(_id);
-  console.log(data);
-
-  const [singleBlog, setSingleBlog] = useState<SingleBlog[]>([]);
-
-  useEffect(() => {
-    fetchData("/db/best-blogs.json")
-      .then((data) => setSingleBlog(data))
-      .catch((err) => console.log(err));
-  }, []);
-
-  const findData = singleBlog.find((blog: any) => blog._id == _id);
+  const {data: blog, isLoading} = useGetBlogByIdQuery(_id);
 
   return (
     <Main>
       <Container>
-        <div className=" sm:mx-16 md:mx-64">
-          <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-              <img
-                className="rounded-t-lg w-full h-72"
-                src={findData?.thumbnail}
-                alt=""
-              />
-            </a>
-            <div className="p-5">
+        {isLoading ? (
+          <HashSpinner />
+        ) : (
+          <div className=" sm:mx-16 md:mx-64">
+            <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {findData?.title}
-                </h5>
+                <img
+                  className="rounded-t-lg w-full h-72"
+                  src={blog?.thumbnail}
+                  alt=""
+                />
               </a>
-              <div className="card-actions py-2 flex justify-between items-center">
-                <div className="flex gap-2 justify-center items-center">
-                  <div>
-                    <img
-                      className="rounded-full w-9 h-9"
-                      src={findData?.userProfile}
-                      alt=""
-                    />
-                  </div>
-                  <div className="flex-col items-center">
-                    <div className="badge badge-outline font-semibold">
-                      <Link to="">{findData?.userName}</Link>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {blog?.title}
+                  </h5>
+                </a>
+                <div className="card-actions py-2 flex justify-between items-center">
+                  <div className="flex gap-2 justify-center items-center">
+                    <div>
+                      <img
+                        className="rounded-full w-9 h-9"
+                        src={blog?.userProfile}
+                        alt=""
+                      />
                     </div>
-                    <div className="badge badge-secondary text-sm">
-                      {findData?.publishDate}
+                    <div className="flex-col items-center">
+                      <div className="badge badge-outline font-semibold">
+                        <Link to="">{blog?.userName}</Link>
+                      </div>
+                      <div className="badge badge-secondary text-sm">
+                        {blog?.publishDate}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {findData?.description}
-              </p>
-              <div className="flex justify-between items-center">
-                <div className="flex items-center gap-2">
-                  <button className="btn btn-primary">
-                    <FcLikePlaceholder className="text-3xl" />
-                  </button>
-                  <span className="text-sm">{findData?.likes} Likes</span>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {blog?.description}
+                </p>
+                <div className="flex justify-between items-center">
+                  <div className="flex items-center gap-2">
+                    <button className="btn btn-primary">
+                      <FcLikePlaceholder className="text-3xl" />
+                    </button>
+                    <span className="text-sm">{blog?.likeCount} Likes</span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </Container>
     </Main>
   );
