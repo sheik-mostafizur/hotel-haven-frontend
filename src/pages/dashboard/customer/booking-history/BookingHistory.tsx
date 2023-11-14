54d75c8b8d5a6aab6adf3fef66954fa30de0510f
eshansaif <eshansaif1234@gmail.com>
2023-11-14 17:20:37 +0600

    payment page is design is updated

--- src/pages/dashboard/customer/booking-history/BookingHistory.tsx
+++ src/pages/dashboard/customer/booking-history/BookingHistory.tsx
@@ -1,6 +1,7 @@
 import SetTitle from "../../../../components/set-title";
 import Button from "../../../../components/ui/button";
 import Container from "../../../../components/ui/container";
+import Modal from "../../../../components/ui/modal";
 
 const BookingHistory = () => {
   return (
@@ -12,16 +13,22 @@ const BookingHistory = () => {
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Hotel Image
+                Booking ID
               </th>
               <th scope="col" className="px-6 py-3">
                 Hotel name
               </th>
               <th scope="col" className="px-6 py-3">
-                Room
+                Location
               </th>
               <th scope="col" className="px-6 py-3">
-                Transaction
+                Room(s)
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Number of Guests
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Staying Duration
               </th>
               <th scope="col" className="px-6 py-3">
                 Payment History
@@ -34,12 +41,28 @@ const BookingHistory = () => {
                 scope="row"
                 className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Image
+                vfdsf12JHGH
               </th>
-              <td className="px-6 py-4">Demo Hotel</td>
-              <td className="px-6 py-4">Demo Room</td>
-              <td className="px-6 py-4">Demo Room</td>
-              <td className="px-6 py-4">Demo Room</td>
+              <td className="px-6 py-4">Test Hotel</td>
+              <td className="px-6 py-4">Cox Bazar</td>
+              <td className="px-6 py-4">`Demo Rooms`</td>
+              <td className="px-6 py-4">2 Adults 1 Child</td>
+              <td className="px-6 py-4">
+                12-Oct-2023 to 14-Oct-2023 (3 Nights)
+              </td>
+              <td className="px-6 py-4">
+                <Modal
+                  title={"See Payment Details"}
+                  button={{
+                    label: "Payment Details",
+                    className: "block ml-auto",
+                  }}
+                >
+                  <p>Transaction id: ewrewtewtwet3535</p>
+                  <p>Payment Method: BKash</p>
+                  <p>Amount: 3000 BDt</p>
+                </Modal>
+              </td>
               {/* <td className="px-6 py-4">
                 <Button size="sm">View Payment Details</Button>
               </td> */}
