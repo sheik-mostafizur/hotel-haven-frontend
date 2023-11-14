039211268c2074d0176b6868a0b7958a118f7f03
AbuBokorprog <abubokor1066@gmail.com>
2023-11-14 17:13:22 +0600

    privacy, terms, license design improved

--- src/pages/license/index.tsx
+++ src/pages/license/index.tsx
@@ -7,34 +7,45 @@ const License: React.FC = () => {
   return (
     <Main>
       <SetTitle title={`License`} />
-      <Container>
+      <Container className="bg-white p-8 rounded-lg shadow-lg">
         <div className="my-10">
-          <h1 className="my-2">Licensing Information</h1>
+          <h1 className="text-4xl font-extrabold text-primary-400 mb-6">
+            Licensing Information
+          </h1>
 
-          <p>
-            Welcome to Hotel Haven, a hotel booking service located in Dhaka,
-            Bangladesh. Here, we provide information about the licensing and
-            registration of our business.
+          <p className="text-gray-700 mb-6">
+            Welcome to Hotel Haven, an elegant hotel booking service located in
+            Dhaka, Bangladesh. Here, we provide detailed information about the
+            licensing and registration of our business.
           </p>
 
-          <h2 className="my-2">Business Information</h2>
+          <h2 className="text-2xl font-bold text-primary-400 mb-4">
+            Business Information
+          </h2>
 
-          <p>
-            <strong>Business Name:</strong> Hotel Haven
-          </p>
-          <p>
-            <strong>Email:</strong> hotelhaven@gmail.com
-          </p>
-          <p>
-            <strong>Location:</strong> Dhaka, Bangladesh
-          </p>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <p className="text-gray-700">
+                <strong>Business Name:</strong> Hotel Haven
+              </p>
+              <p className="text-gray-700">
+                <strong>Email:</strong> hotelhaven@gmail.com
+              </p>
+              <p className="text-gray-700">
+                <strong>Location:</strong> Dhaka, Bangladesh
+              </p>
+            </div>
+          </div>
 
-          <h2 className="my-2">Licensing and Registration</h2>
+          <h2 className="text-2xl font-bold text-primary-400 my-4">
+            Licensing and Registration
+          </h2>
 
-          <p>
+          <p className="text-gray-700">
             Hotel Haven is a legally registered and licensed business in Dhaka,
-            Bangladesh. We adhere to all relevant regulations and requirements
-            as set forth by the local authorities.
+            Bangladesh. We meticulously adhere to all relevant regulations and
+            requirements as set forth by the local authorities, ensuring a
+            sophisticated and secure experience.
           </p>
         </div>
       </Container>
--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -7,94 +7,98 @@ const PrivacyPolicy: React.FC = () => {
   return (
     <Main>
       <SetTitle title={`Privacy Policy`} />
-      <Container>
-        <div className="my-10">
-          <h1 className="my-2">Privacy Policy</h1>
+      <Container className="bg-gray-100 p-8 rounded-lg shadow-lg">
+        <div className="flex flex-col items-center my-10">
+          <h1 className="text-4xl font-extrabold text-primary-400 mb-4">
+            Privacy Policy
+          </h1>
 
-          <p>
+          <p className="text-gray-700 text-center mb-8">
             This Privacy Policy describes how Hotel Haven ("we," "us," or "our")
             collects, uses, and shares your personal information when you use
             our hotel booking website.
           </p>
+        </div>
 
-          <h2 className="my-2">Information We Collect</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold text-primary-400 mb-4">
+              Information We Collect
+            </h2>
+            <ul className="list-disc list-inside text-gray-700">
+              <li>
+                Your name, email address, and contact information when you make
+                a reservation.
+              </li>
+              <li>Payment information when you make a booking.</li>
+              <li>
+                Information about your stay, such as check-in and check-out
+                dates, room preferences, and special requests.
+              </li>
+              <li>
+                Information you provide when you contact us for customer support
+                or other inquiries.
+              </li>
+              <li>
+                Information collected through cookies and similar technologies
+                when you visit our website (please see our Cookie Policy for
+                more details).
+              </li>
+            </ul>
+          </div>
 
-          <p>
-            When you use our website, we may collect the following types of
-            information:
-          </p>
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold text-primary-400 mb-4">
+              How We Use Your Information
+            </h2>
+            <ul className="list-disc list-inside text-gray-700">
+              <li>Processing and confirming your reservations.</li>
+              <li>
+                Providing customer support and responding to your inquiries.
+              </li>
+              <li>Improving and optimizing our website and services.</li>
+              <li>
+                Communicating with you about promotions, special offers, and
+                other relevant information.
+              </li>
+              <li>Complying with legal and regulatory requirements.</li>
+            </ul>
+          </div>
+        </div>
 
-          <ul>
-            <li>
-              Your name, email address, and contact information when you make a
-              reservation.
-            </li>
-            <li>Payment information when you make a booking.</li>
-            <li>
-              Information about your stay, such as check-in and check-out dates,
-              room preferences, and special requests.
-            </li>
-            <li>
-              Information you provide when you contact us for customer support
-              or other inquiries.
-            </li>
-            <li>
-              Information collected through cookies and similar technologies
-              when you visit our website (please see our Cookie Policy for more
-              details).
-            </li>
-          </ul>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold text-primary-400 mb-4">
+              Sharing Your Information
+            </h2>
+            <ul className="list-disc list-inside text-gray-700">
+              <li>
+                With service providers and partners who help us operate our
+                website and provide services to you.
+              </li>
+              <li>
+                If required by law, such as responding to a legal request or
+                complying with applicable regulations.
+              </li>
+              <li>
+                If we are involved in a merger, acquisition, or sale of all or a
+                portion of our assets, your information may be transferred as
+                part of that transaction.
+              </li>
+            </ul>
+          </div>
 
-          <h2 className="my-2">How We Use Your Information</h2>
-
-          <p>
-            We use the information we collect for various purposes, including:
-          </p>
-
-          <ul>
-            <li>Processing and confirming your reservations.</li>
-            <li>
-              Providing customer support and responding to your inquiries.
-            </li>
-            <li>Improving and optimizing our website and services.</li>
-            <li>
-              Communicating with you about promotions, special offers, and other
-              relevant information.
-            </li>
-            <li>Complying with legal and regulatory requirements.</li>
-          </ul>
-
-          <h2 className="my-2">Sharing Your Information</h2>
-
-          <p>
-            We may share your information with third parties in the following
-            circumstances:
-          </p>
-
-          <ul>
-            <li>
-              With service providers and partners who help us operate our
-              website and provide services to you.
-            </li>
-            <li>
-              If required by law, such as responding to a legal request or
-              complying with applicable regulations.
-            </li>
-            <li>
-              If we are involved in a merger, acquisition, or sale of all or a
-              portion of our assets, your information may be transferred as part
-              of that transaction.
-            </li>
-          </ul>
-
-          <h2 className="my-2">Your Choices</h2>
-
-          <p>
-            You have certain rights regarding your personal information,
-            including the right to access, correct, or delete your data. You can
-            contact us using the information provided in the "Contact Us"
-            section to exercise these rights.
-          </p>
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold text-primary-400 mb-4">
+              Your Choices
+            </h2>
+            <p className="text-gray-700">
+              You have certain rights regarding your personal information,
+              including the right to access, correct, or delete your data. You
+              can contact us using the information provided in the "Contact Us"
+              section to exercise these rights.
+            </p>
+          </div>
         </div>
       </Container>
     </Main>
